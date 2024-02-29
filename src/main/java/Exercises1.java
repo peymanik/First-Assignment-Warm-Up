9beac3d2cd2d668f2392abf4d7be36ae12351ae3
peyman <peymanick@gmail.com>
2024-02-29 22:33:19 +0330

    Exercises1.java

--- src/main/java/Exercises1.java
+++ src/main/java/Exercises1.java
@@ -2,9 +2,17 @@ public class Exercises1 {
     /*
     implement a function that returns factorial of given n
      */
-    public long factorial(int n) {
+    public static long factorial(int n) {
         // TODO
-        return 0;
+        if(n==0){
+            return 1;
+        }
+        long fac = 1;
+        while(n>0){
+            fac = fac*n;
+            n=n-1;
+        }
+        return fac;
     }
 
 
@@ -14,7 +22,20 @@ public class Exercises1 {
     */
     public long fibonacci(int n) {
         // TODO
-        return 0;
+        if( n<3){
+            return 1;
+        } else{
+            long current_num = 1;
+            long past_num = 1;
+            long current_num_copy;
+            for(int i=1 ; i<n-1 ; i++){
+                current_num_copy = current_num;
+                current_num = current_num + past_num;
+                past_num = current_num_copy;
+            }
+            return current_num;
+        }
+
     }
 
 
@@ -31,12 +52,24 @@ public class Exercises1 {
      */
     public char[][] generateTriangle(int rows) {
         // TODO
-        return null;
+        char[][] triangle = new char[rows][];
+        for(int i=0 ; i<rows ; i++){
+            triangle[i] = new char[i+1];
+        }
+        for(int i=0 ; i<rows ; i++){
+            for(int j=0 ; j<i+1 ; j++){
+                triangle[i][j] = '*';
+            }
+        }
+
+        return triangle;
     }
 
 
     public static void main(String[] args) {
+
         // test your code here!
+
     }
 
 }
